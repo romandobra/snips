c98a986706d849ba056f45da81e179d0c3f5e6f2
Roman Dobra <romanreachless@gmail.com>
2023-12-01 14:46:18 +0000

    _rd version

--- _rd.sh
+++ _rd.sh
@@ -5,10 +5,11 @@
 rd_url='https://raw.githubusercontent.com/romandobra/snips/main'
 
 [ -z $3 ] || {
-    wget -qO - $rd_url/_rd.sh | grep "# version" | cut -d':' -f2-
+    wget --no-cache --no-cookies -qO - $rd_url/_rd.sh | grep "# version" | cut -d':' -f2-
 }
 
 exit 0
 
 #https://api.github.com/repos/romandobra/snips/git/trees/master
 #https://api.github.com/repos/{owner}/{repo}/contents/{path}
+
