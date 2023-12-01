672795f183c9f32faebf2d1e845a971c38a52c48
Roman Dobra <romanreachless@gmail.com>
2023-12-01 14:54:37 +0000

    version

--- _rd.sh
+++ _rd.sh
@@ -1,12 +1,12 @@
 #!/bin/env bash
 
-version='0.1.1a'
+rd_version='0.1.1a'
 
 rd_url='https://raw.githubusercontent.com/romandobra/snips/main'
 
 [ -z $3 ] || {
-#    wget --no-cache --no-cookies -qO - $rd_url/_rd.sh | grep -m 1 "version=" | cut -d"'" -f2
-    wget --no-cache --no-cookies -qO - $rd_url/_rd.sh | grep -m 1 "version=" | cut -d"'" -f2
+    rd_new_version=$( wget --no-cache --no-cookies -qO - $rd_url/_rd.sh | grep -m 1 "rd_version=" | cut -d"'" -f2; )
+    echo $rd_new_version
 }
 
 exit 0
