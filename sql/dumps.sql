caf28a893aaa21e06d5d7be59bc91fcb381b3f3d
Roman Dobra <romanreachless@gmail.com>
2023-12-01 15:50:48 +0000

    err

--- sql/dumps.sql
+++ sql/dumps.sql
@@ -2,7 +2,7 @@
 SELECT table_name, table_rows INTO OUTFILE '/var/lib/mysql-files/tables-with-recods.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' FROM INFORMATION_SCHEMA.TABLES WHERE table_rows>0;
 
 -- bash script to search for a PATTERN in db
--- mysqldump -pPASSWORD database | grep PATTERN | less -S
+-- mysqldump database | grep PATTERN | less -S
 
 -- new root password
 UPDATE mysql.user SET authentication_string = PASSWORD('password') WHERE User = 'root' AND Host = 'localhost';
