8e22365406f13f63d7591980489970d3be729a31
Roman Dobra <romanreachless@gmail.com>
2023-12-01 18:26:57 +0000

    update system

--- _rd.sh
+++ _rd.sh
@@ -4,13 +4,20 @@ rd_version='0.1.1a'
 
 rd_url='https://raw.githubusercontent.com/romandobra/snips/main'
 
-[ -z $3 ] || {
-    rd_new_version=$( wget --no-cache --no-cookies -qO - $rd_url/_rd.sh | grep -m 1 "rd_version=" | cut -d"'" -f2; )
-    echo $rd_new_version
+wget --no-cache --no-cookies -qO /tmp/_rd_new.sh $rd_url/_rd.sh
+rd_new_version=$( cat /tmp/_rd_new.sh | grep -m 1 "rd_version=" | cut -d"'" -f2; )
+    
+[ $rd_new_version == $rd_version ] || {
+    read -p "Update $0 from $rd_version to $rd_new_version? [(n)|y]: "
+    rd_update=${REPLY:-n}
+    [ $rd_update == y ] && {
+        echo "Updating..."
+        (
+            sleep 1; cat /tmp/_rd_new.sh > $0; echo; echo "Done! Please restart '$0 $@'"; rm -rf /tmp/_rd_new.sh
+        ) &
+        exit 0
+    }
+    rm -rf /tmp/_rd_new.sh
 }
 
 exit 0
-
-#https://api.github.com/repos/romandobra/snips/git/trees/master
-#https://api.github.com/repos/{owner}/{repo}/contents/{path}
-
