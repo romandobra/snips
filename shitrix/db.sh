d924315f299713622e21d9034b3ccb8610a9930b
Roman Dobra <romanreachless@gmail.com>
2023-12-01 12:59:59 +0000

    add

---
+++ shitrix/db.sh
@@ -0,0 +1,6 @@
+exit 1
+
+# passwords
+cat /bitrix/php_interface/dbconn.php
+cat /bitrix/.settings.php
+
--- sql/dumps.sql
+++ sql/dumps.sql
@@ -1,3 +1,5 @@
 -- select all not empty tables into csv file
 SELECT table_name, table_rows INTO OUTFILE '/var/lib/mysql-files/tables-with-recods.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' FROM INFORMATION_SCHEMA.TABLES WHERE table_rows>0;
 
+-- bash script to search for a PATTERN in db
+-- mysqldump -pPASSWORD database | grep PATTERN | less -S
